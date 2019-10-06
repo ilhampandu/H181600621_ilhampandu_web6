8ab762b293a8820f031561b3ae7778cee1705e5d
ilhampandu9@gmail.com <vinkietha@gmail.com>
2019-10-06 22:59:39 +0800

    web6

---
+++ app/Http/Controllers/HelperController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelperController extends Controller
+{
+    function refereshCapcha(){
+        return captcha_img('flat');
+
+    }
+}
